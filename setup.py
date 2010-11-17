35ea79d1fd6c730a1882d504cbceaed6bfc2461c
Marcel Martin <marcel.martin@tu-dortmund.de>
2010-11-17 18:07:37 +0000

    change version to 0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup, Extension
 module = Extension('calign', sources = [ 'calignmodule.c' ])
 
 setup(name = 'cutadapt',
-	version = '0.4',
+	version = '0.5'
 	description = 'trim adapters from high-throughput sequencing reads',
 	author = 'Marcel Martin',
 	author_email = 'marcel.martin@tu-dortmund.de',
