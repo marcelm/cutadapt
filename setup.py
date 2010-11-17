55cdf9ecae2a9d2c9357a96304234a9e5b7d341f
Marcel Martin <marcel.martin@tu-dortmund.de>
2010-11-17 15:25:13 +0000

    add more meta-information to setup script

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@ from distutils.core import setup, Extension
 
 module = Extension('calign', sources = [ 'calignmodule.c' ])
 
-setup (name = 'cutadapt',
-	version = '0.2',
+setup(name = 'cutadapt',
+	version = '0.3',
 	description = 'trim adapters from high-throughput sequencing reads',
+	author = 'Marcel Martin',
+	author_email = 'marcel.martin@tu-dortmund.de',
+	url = 'http://cutadapt.googlecode.com/',
+	license = 'MIT',
 	ext_modules = [ module ],
-	py_modules = ['fasta' ],
-	scripts = ['cutadapt' ]
+	py_modules = ['fasta'],
+	scripts = ['cutadapt']
 )
