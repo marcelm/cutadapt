381aef793ef4b4ad3f096ad4b244b0ce68710d3c
Marcel Martin <marcel.martin@tu-dortmund.de>
2010-11-25 16:27:00 +0000

    revert cython changes

--- setup.py
+++ setup.py
@@ -1,19 +1,15 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from distutils.core import setup, Extension
 
-#calign = Extension('calign', sources = [ 'calignmodule.c' ])
-pyxalign = Extension("align", ["align.pyx"])
+module = Extension('calign', sources = [ 'calignmodule.c' ])
 
 setup(name = 'cutadapt',
-	version = '0.5.1',
+	version = '0.6',
 	description = 'trim adapters from high-throughput sequencing reads',
 	author = 'Marcel Martin',
 	author_email = 'marcel.martin@tu-dortmund.de',
 	url = 'http://cutadapt.googlecode.com/',
 	license = 'MIT',
-	cmdclass = {'build_ext': build_ext},
-	ext_modules = [ pyxalign ],
+	ext_modules = [ module ],
 	py_modules = ['fasta'],
 	scripts = ['cutadapt']
 )
