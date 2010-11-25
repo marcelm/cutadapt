dfc6de17d462569cc1f46d4724ec1fa411643294
Marcel Martin <marcel.martin@tu-dortmund.de>
2010-11-25 15:30:45 +0000

    trying to use cython: starting with Python version of the alignment module

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
-from distutils.core import setup, Extension
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
 
-module = Extension('calign', sources = [ 'calignmodule.c' ])
+#calign = Extension('calign', sources = [ 'calignmodule.c' ])
+pyxalign = Extension("align", ["align.pyx"])
 
 setup(name = 'cutadapt',
-	version = '0.6',
+	version = '0.5.1',
 	description = 'trim adapters from high-throughput sequencing reads',
 	author = 'Marcel Martin',
 	author_email = 'marcel.martin@tu-dortmund.de',
 	url = 'http://cutadapt.googlecode.com/',
 	license = 'MIT',
-	ext_modules = [ module ],
+	cmdclass = {'build_ext': build_ext},
+	ext_modules = [ pyxalign ],
 	py_modules = ['fasta'],
 	scripts = ['cutadapt']
 )
