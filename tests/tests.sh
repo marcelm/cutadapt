38f920063cb0a9ee0974c379188f292f2111bcf8
Marcel Martin <marcel.martin@tu-dortmund.de>
2012-01-07 21:26:43 +0000

    remove duplicated lines

--- tests/tests.sh
+++ tests/tests.sh
@@ -112,12 +112,6 @@ test_cutadapt "-b TTAGACATATCTCCGTCG" small.fastq multiblock.fastq.gz
 # -y/--suffix parameter, combined with _F3
 test_cutadapt "-c -e 0.12 -a 330201030313112312 -y _my_suffix --strip-f3" suffix.fastq solid.csfasta solid.qual
 
-# compressed gz file with multiple blocks (created by concatenating two .gz files)
-test_cutadapt "-b TTAGACATATCTCCGTCG" small.fastq multiblock.fastq.gz
-
-# -y/--suffix parameter, combined with _F3
-test_cutadapt "-c -e 0.12 -a 330201030313112312 -y _my_suffix --strip-f3" suffix.fastq solid.csfasta solid.qual
-
 # test read wildcards
 test_cutadapt "--match-read-wildcards -b ACGTACGT" wildcard.fa wildcard.fa
 
