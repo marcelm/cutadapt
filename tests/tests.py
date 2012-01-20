3028a1150e5b106d0e05698ae75d8713ca0ec867
Marcel Martin <marcel.martin@tu-dortmund.de>
2012-01-20 17:10:59 +0000

    port the remaining tests from tests.sh

--- tests/tests.py
+++ tests/tests.py
@@ -1,3 +1,4 @@
+from __future__ import print_function, division
 import os
 import imp
 
@@ -13,13 +14,21 @@ def dpath(path):
 	test lives in)"""
 	return os.path.join(os.path.dirname(__file__), path)
 
+def datapath(path):
+	return dpath(os.path.join('data', path))
+
+
+def diff(path1, path2):
+	assert os.system("diff -u {} {}".format(path1, path2)) == 0
+
 
 def run(params, expected, inpath, inpath2=None):
+	if type(params) is str:
+		params = params.split()
 	params += ['-o', dpath('tmp.fastaq') ] # TODO not parallelizable
-	params += [ dpath(os.path.join('data', inpath)) ]
+	params += [ datapath(inpath) ]
 	if inpath2:
-		params += [ dpath(os.path.join('data', inpath2)) ]
-	print "params:", params
+		params += [ datapath(inpath2) ]
 		
 	ca.main(params)
 	# TODO redirect standard output
@@ -55,114 +64,127 @@ def test_lowercase():
 	'''lower case adapter'''
 	run("-b ttagacatatctccgtcg", "lowercase.fastq", "small.fastq")
 
+
 def test_rest():
 	'''-r/--rest-file'''
-	assert False
-	run("-b ADAPTER -r rest.tmp", "rest.fa", "rest.fa")
-	#diff -u rest.tmp data/rest.txt
-	#rm rest.tmp
+	run(['-b', 'ADAPTER', '-r', dpath('rest.tmp')], "rest.fa", "rest.fa")
+	diff(dpath('rest.tmp'), datapath('rest.txt'))
+	os.remove(dpath('rest.tmp'))
 
 
 def test_restfront():
-	run("-g ADAPTER -r rest.tmp", "restfront.fa", "rest.fa")
-	assert False
-	#diff -u rest.tmp data/restfront.txt
-	#rm rest.tmp
+	run(['-g', 'ADAPTER', '-r', dpath('rest.tmp')], "restfront.fa", "rest.fa")
+	diff(dpath('rest.tmp'), datapath('restfront.txt'))
+	os.remove(dpath('rest.tmp'))
+
 
 def test_discard():
 	'''--discard'''
 	run("-b TTAGACATATCTCCGTCG --discard", "discard.fastq", "small.fastq")
 
+
 def test_plus():
 	'''test if sequence name after the "+" is retained'''
 	run("-e 0.12 -b TTAGACATATCTCCGTCG", "plus.fastq", "plus.fastq")
+
 
 def test_extensiontxtgz():
 	'''automatic recognition of "_sequence.txt.gz" extension'''
 	run("-b TTAGACATATCTCCGTCG", "s_1_sequence.txt", "s_1_sequence.txt.gz")
 
+
 def test_format():
 	'''the -f/--format parameter'''
 	run("-f fastq -b TTAGACATATCTCCGTCG", "small.fastq", "small.myownextension")
+
 
 def test_minimum_length():
 	'''-m/--minimum-length'''
 	run("-c -m 5 -a 330201030313112312", "minlen.fa", "minlen.fa")
 
+
 def test_too_short():
 	'''--too-short-output'''
-	assert False
 	run("-c -m 5 -a 330201030313112312 --too-short-output tooshort.tmp.fa", "minlen.fa", "minlen.fa")
-	diff -u data/tooshort.fa tooshort.tmp.fa
-	rm tooshort.tmp.fa
+	diff(datapath('tooshort.fa'), "tooshort.tmp.fa")
+	os.remove('tooshort.tmp.fa')
+
 
 def test_maximum_length():
 	'''-M/--maximum-length'''
 	run("-c -M 5 -a 330201030313112312", "maxlen.fa", "maxlen.fa")
 
+
 def test_length_tag():
 	'''454 data; -n and --length-tag'''
-	assert False
-	test_cutadapt "-n 3 -e 0.1 --length-tag length=
-	-b TGAGACACGCAACAGGGGAAAGGCAAGGCACACAGGGGATAGG
-	-b TCCATCTCATCCCTGCGTGTCCCATCTGTTCCCTCCCTGTCTCA" 454.fa 454.fa
+	run("-n 3 -e 0.1 --length-tag length= " \
+		"-b TGAGACACGCAACAGGGGAAAGGCAAGGCACACAGGGGATAGG "\
+		"-b TCCATCTCATCCCTGCGTGTCCCATCTGTTCCCTCCCTGTCTCA", '454.fa', '454.fa')
 
-def test_overlap():
+
+def test_overlap_a():
 	'''-O/--overlap with -a (-c omitted on purpose)'''
 	run("-O 10 -a 330201030313112312", "overlapa.fa", "overlapa.fa")
 
-'''
-# -O/--overlap with -b
+def test_overlap_b():
+	'''-O/--overlap with -b'''
 	run("-O 10 -b TTAGACATATCTCCGTCG", "overlapb.fa", "overlapb.fa")
 
-# -q with low qualities
+def test_qualtrim():
+	'''-q with low qualities'''
 	run("-q 10 -a XXXXXX", "lowqual.fastq", "lowqual.fastq")
 
-# -q with low qualities, using ascii(quality+64) encoding
+def test_qualbase():
+	'''-q with low qualities, using ascii(quality+64) encoding'''
 	run("-q 10 --quality-base 64 -a XXXXXX", "illumina64.fastq", "illumina64.fastq")
 
-# two adapters
+def test_twoadapters():
+	'''two adapters'''
 	run("-b CTCGAGAATTCTGGATCCTC -b GAGGATCCAGAATTCTCGAGTT", "twoadapters.fasta", "twoadapters.fasta")
 
-# MAQ-compatible output
-	run("-c -e 0.12 -a 330201030313112312 -x 552: --maq", "solidmaq.fastq", "solid.csfasta") solid.qual
+def test_bwa():
+	'''MAQ-/BWA-compatible output'''
+	run("-c -e 0.12 -a 330201030313112312 -x 552: --maq", "solidmaq.fastq", "solid.csfasta", 'solid.qual')
 
-# BFAST-compatible output
-	run("-c -e 0.12 -a 330201030313112312 -x abc: --strip-f3", "solidbfast.fastq", "solid.csfasta") solid.qual
+def test_bfast():
+	'''BFAST-compatible output'''
+	run("-c -e 0.12 -a 330201030313112312 -x abc: --strip-f3", "solidbfast.fastq", "solid.csfasta", 'solid.qual')
 
-# poly-A tails
+def test_polya():
+	'''poly-A tails'''
 	run("-m 24 -O 10 -a AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", "polya.fasta", "polya.fasta")
 
-# some reads properly trimmed since cutadapt 0.9.5
+def test_trim_095():
+	'''some reads properly trimmed since cutadapt 0.9.5'''
 	run("-c -e 0.122 -a 330201030313112312", "solid.fasta", "solid.fasta")
 
-# compressed gz file with multiple blocks (created by concatenating two .gz files)
+def test_gz_multiblock():
+	'''compressed gz file with multiple blocks (created by concatenating two .gz files)'''
 	run("-b TTAGACATATCTCCGTCG", "small.fastq", "multiblock.fastq.gz")
 
-# -y/--suffix parameter, combined with _F3
-	run("-c -e 0.12 -a 330201030313112312 -y _my_suffix --strip-f3", "suffix.fastq", "solid.csfasta") solid.qual
+def test_suffix():
+	'''-y/--suffix parameter, combined with _F3'''
+	run("-c -e 0.12 -a 330201030313112312 -y _my_suffix --strip-f3", "suffix.fastq", "solid.csfasta", 'solid.qual')
 
-# compressed gz file with multiple blocks (created by concatenating two .gz files)
-	run("-b TTAGACATATCTCCGTCG", "small.fastq", "multiblock.fastq.gz")
-
-# -y/--suffix parameter, combined with _F3
-	run("-c -e 0.12 -a 330201030313112312 -y _my_suffix --strip-f3", "suffix.fastq", "solid.csfasta") solid.qual
-
-# test read wildcards
+def test_read_wildcard():
+	'''test wildcards in reads'''
 	run("--match-read-wildcards -b ACGTACGT", "wildcard.fa", "wildcard.fa")
 
-# test adapter wildcards
+def test_adapter_wildcard():
+	'''test wildcards in adapter'''
 	run("--wildcard-file - -b ACGTNNNACGT", "wildcard_adapter.fa", "wildcard_adapter.fa")
 
-# test 'N' wildcard matching with no allowed errors=
+def test_wildcard_N():
+	'''test 'N' wildcard matching with no allowed errors='''
 	run("-e 0 -a GGGGGGG --match-read-wildcards", "wildcardN.fa", "wildcardN.fa")
 
-# test adapter in front
+def test_adapter_front():
+	'''test adapter in front'''
 	run("--front ADAPTER", "examplefront.fa", "example.fa")
 
-# test matching literal 'N's
+def test_literal_N():
+	'''test matching literal 'N's'''
 	run("-N -e 0.2 -a NNNNNNNNNNNNNN", "trimN3.fasta", "trimN3.fasta")
 
+def test_literal_N2():
 	run("-N -O 1 -g NNNNNNNNNNNNNN", "trimN5.fasta", "trimN5.fasta")
-
-'''
