54dc27c45ebe17bfc5b3d9c336926ff690551f00
Marcel Martin <marcel.martin@scilifelab.se>
2014-03-26 09:14:09 +0100

    at install time, check whether the found Cython is recent enough

--- setup.py
+++ setup.py
@@ -1,21 +1,32 @@
-from distutils.core import setup, Extension
 import sys
 import os.path
+from distutils.core import setup, Extension
+from distutils.version import StrictVersion
 
 from cutadapt import __version__
+
+MIN_CYTHON_VERSION = '0.15'
 
 if sys.version_info < (2, 6):
 	sys.stdout.write("At least Python 2.6 is required.\n")
 	sys.exit(1)
 
+# Try to find out whether a recent enough Cython is installed.
+# If it is not, fall back to using the pre-compiled C sources.
+# Pre-compiled sources are available only in the official releases,
+# not within the Git repository.
 try:
-	from Cython.Distutils import build_ext
-	USE_CYTHON = True
+	from Cython import __version__ as cyversion
+	USE_CYTHON = StrictVersion(cyversion) >= StrictVersion(MIN_CYTHON_VERSION)
 except ImportError:
+	cyversion = None
 	USE_CYTHON = False
-	cmdclass = { }
-else:
+
+if USE_CYTHON:
+	from Cython.Distutils import build_ext
 	cmdclass = { 'build_ext' : build_ext }
+else:
+	cmdclass = { }
 
 ext = '.pyx' if USE_CYTHON else '.c'
 
@@ -28,8 +39,11 @@ extensions = [
 if not USE_CYTHON:
 	for extension in extensions:
 		if not os.path.exists(extension.sources[0]):
-			sys.stdout.write("Cython is not installed and pre-compiled C sources\n")
+			sys.stdout.write("Error:\n")
+			sys.stdout.write("Cython version " + str(MIN_CYTHON_VERSION) + " or later is not installed and pre-compiled C sources\n")
 			sys.stdout.write("are also not available. You need to install Cython to continue.\n")
+			if cyversion:
+				sys.stdout.write("Found Cython version: " + cyversion + "\n")
 			sys.exit(1)
 
 setup(
