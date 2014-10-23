740be8f67e9d99802975e5e9f99c559d1e835be1
Marcel Martin <marcel.martin@scilifelab.se>
2014-10-23 14:26:56 +0200

    Require Cython if .c files are out of date

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 """
 Build cutadapt.
 
-If pre-generated .c extension files are found, then Cython is not run, even if it is installed, unless --cython is given
-on the command line.
-
-If .c files are not found, then Cython is always run. This is the case, for example, when using a fresh Git checkout.
+Cython is run when
+* no pre-generated C sources are found,
+* or the pre-generated C sources are out of date,
+* or when --cython is given on the command line.
 """
 import sys
 import os.path
@@ -13,48 +13,82 @@ from distutils.version import LooseVersion
 
 from cutadapt import __version__
 
-MIN_CYTHON_VERSION = '0.15'
+MIN_CYTHON_VERSION = '0.17'
 
 if sys.version_info < (2, 6):
 	sys.stdout.write("At least Python 2.6 is required.\n")
 	sys.exit(1)
 
 
-use_cython = not os.path.exists('cutadapt/_align.c')
+def out_of_date(extensions):
+	"""
+	Check whether any pyx source is newer than the corresponding generated
+	C source or whether any C source is missing.
+	"""
+	for extension in extensions:
+		for pyx in extension.sources:
+			path, ext = os.path.splitext(pyx)
+			if ext not in ('.pyx', '.py'):
+				continue
+			csource = path + ('.cpp' if extension.language == 'c++' else '.c')
+			if not os.path.exists(csource) or (
+				os.path.getmtime(pyx) > os.path.getmtime(csource)):
+				return True
+	return False
 
-if '--cython' in sys.argv:
-	use_cython = True
-	sys.argv.remove('--cython')
 
-# Try to find out whether a recent enough Cython is installed.
-# If it is not, fall back to using the pre-compiled C sources.
-# Pre-compiled sources are available only in the official releases,
-# not within the Git repository.
-if use_cython:
+def no_cythonize(extensions, **_ignore):
+	"""
+	Change file extensions from .pyx to .c or .cpp.
+
+	Copied from Cython documentation
+	"""
+	for extension in extensions:
+		sources = []
+		for sfile in extension.sources:
+			path, ext = os.path.splitext(sfile)
+			if ext in ('.pyx', '.py'):
+				if extension.language == 'c++':
+					ext = '.cpp'
+				else:
+					ext = '.c'
+				sfile = path + ext
+			sources.append(sfile)
+		extension.sources[:] = sources
+	return extensions
+
+
+def cythonize_if_necessary(extensions):
+	if '--cython' in sys.argv:
+		sys.argv.remove('--cython')
+	elif out_of_date(extensions):
+		sys.stdout.write('At least one C source file is missing or out of date.\n')
+	else:
+		return no_cythonize(extensions)
+
 	try:
 		from Cython import __version__ as cyversion
 	except ImportError:
 		sys.stdout.write(
-			"ERROR: Cython is not installed. Install at least Cython >= " + str(MIN_CYTHON_VERSION) +
-		    " to continue.\n")
+			"ERROR: Cython is not installed. Install at least Cython version " +
+			str(MIN_CYTHON_VERSION) + " to continue.\n")
 		sys.exit(1)
 	if LooseVersion(cyversion) < LooseVersion(MIN_CYTHON_VERSION):
 		sys.stdout.write(
-			"Error: Your Cython is at version '" + str(cyversion) +
+			"ERROR: Your Cython is at version '" + str(cyversion) +
 			"', but at least version " + str(MIN_CYTHON_VERSION) + " is required.\n")
 		sys.exit(1)
 
 	from Cython.Build import cythonize
+	return cythonize(extensions)
 
-ext = '.pyx' if use_cython else '.c'
 
 extensions = [
-	Extension('cutadapt._align', sources=['cutadapt/_align' + ext]),
-	Extension('cutadapt._qualtrim', sources=['cutadapt/_qualtrim' + ext]),
-	Extension('cutadapt._seqio', sources=['cutadapt/_seqio' + ext]),
+	Extension('cutadapt._align', sources=['cutadapt/_align.pyx']),
+	Extension('cutadapt._qualtrim', sources=['cutadapt/_qualtrim.pyx']),
+	Extension('cutadapt._seqio', sources=['cutadapt/_seqio.pyx']),
 ]
-if use_cython:
-	extensions = cythonize(extensions)
+extensions = cythonize_if_necessary(extensions)
 
 setup(
 	name = 'cutadapt',
