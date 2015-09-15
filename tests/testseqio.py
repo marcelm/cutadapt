938ed4375c65ac5385a5745edc39f45cd9c32266
Marcel Martin <marcel.martin@scilifelab.se>
2015-09-15 13:33:30 +0200

    Put tests into classes to add some structure

--- tests/testseqio.py
+++ tests/testseqio.py
@@ -17,176 +17,176 @@ simple_fastq = [
 simple_fasta = [ seqio.Sequence(x.name, x.sequence, None) for x in simple_fastq ]
 
 
-def test_fastareader():
-	with seqio.FastaReader("tests/data/simple.fasta") as f:
-		reads = list(f)
-	assert reads == simple_fasta
+class TestFastaReader:
+	def test_fastareader(self):
+		with seqio.FastaReader("tests/data/simple.fasta") as f:
+			reads = list(f)
+		assert reads == simple_fasta
 
-	fasta = StringIO(">first_sequence\nSEQUENCE1\n>second_sequence\nSEQUENCE2\n")
-	reads = list(seqio.FastaReader(fasta))
-	assert reads == simple_fasta
-
-
-def test_fastareader_with_comments():
-	fasta = StringIO(dedent(
-		"""
-		# a comment
-		# another one
-		>first_sequence
-		SEQUENCE1
-		>second_sequence
-		SEQUENCE2
-		"""))
-	reads = list(seqio.FastaReader(fasta))
-	assert reads == simple_fasta
+		fasta = StringIO(">first_sequence\nSEQUENCE1\n>second_sequence\nSEQUENCE2\n")
+		reads = list(seqio.FastaReader(fasta))
+		assert reads == simple_fasta
 
 
-@raises(seqio.FormatError)
-def test_wrong_fasta_format():
-	fasta = StringIO(dedent(
-		"""
-		# a comment
-		# another one
-		unexpected
-		>first_sequence
-		SEQUENCE1
-		>second_sequence
-		SEQUENCE2
-		"""))
-	reads = list(seqio.FastaReader(fasta))
+	def test_with_comments(self):
+		fasta = StringIO(dedent(
+			"""
+			# a comment
+			# another one
+			>first_sequence
+			SEQUENCE1
+			>second_sequence
+			SEQUENCE2
+			"""))
+		reads = list(seqio.FastaReader(fasta))
+		assert reads == simple_fasta
 
 
-def test_fastqreader():
-	with seqio.FastqReader("tests/data/simple.fastq") as f:
-		reads = list(f)
-	assert reads == simple_fastq
+	@raises(seqio.FormatError)
+	def test_wrong_format(self):
+		fasta = StringIO(dedent(
+			"""
+			# a comment
+			# another one
+			unexpected
+			>first_sequence
+			SEQUENCE1
+			>second_sequence
+			SEQUENCE2
+			"""))
+		reads = list(seqio.FastaReader(fasta))
+
+	def test_fastareader_keeplinebreaks(self):
+		with seqio.FastaReader("tests/data/simple.fasta", keep_linebreaks=True) as f:
+			reads = list(f)
+		assert reads[0] == simple_fasta[0]
+		assert reads[1].sequence == 'SEQUEN\nCE2'
+
+	def test_fasta_context_manager(self):
+		filename = "tests/data/simple.fasta"
+		with open(filename) as f:
+			assert not f.closed
+			reads = list(seqio.open(f))
+			assert not f.closed
+		assert f.closed
+
+		with seqio.FastaReader(filename) as sr:
+			tmp_sr = sr
+			assert not sr.fp.closed
+			reads = list(sr)
+			assert not sr.fp.closed
+		assert tmp_sr.fp is None
 
 
-def test_fastqreader_dos():
-	with seqio.FastqReader("tests/data/dos.fastq") as f:
-		dos_reads = list(f)
-	with seqio.FastqReader("tests/data/small.fastq") as f:
-		unix_reads = list(f)
-	assert dos_reads == unix_reads
+class TestFastqReader:
+	def test_fastqreader(self):
+		with seqio.FastqReader("tests/data/simple.fastq") as f:
+			reads = list(f)
+		assert reads == simple_fastq
+
+	def test_fastqreader_dos(self):
+		with seqio.FastqReader("tests/data/dos.fastq") as f:
+			dos_reads = list(f)
+		with seqio.FastqReader("tests/data/small.fastq") as f:
+			unix_reads = list(f)
+		assert dos_reads == unix_reads
+
+	@raises(seqio.FormatError)
+	def test_fastq_wrongformat(self):
+		with seqio.FastqReader("tests/data/withplus.fastq") as f:
+			reads = list(f)
+
+	@raises(seqio.FormatError)
+	def test_fastq_incomplete(self):
+		fastq = StringIO("@name\nACGT+\n")
+		with seqio.FastqReader(fastq) as fq:
+			list(fq)
+
+	def test_context_manager(self):
+		filename = "tests/data/simple.fastq"
+		with open(filename) as f:
+			assert not f.closed
+			reads = list(seqio.open(f))
+			assert not f.closed
+		assert f.closed
+
+		with seqio.FastqReader(filename) as sr:
+			tmp_sr = sr
+			assert not sr.fp.closed
+			reads = list(sr)
+			assert not sr.fp.closed
+		assert tmp_sr.fp is None
 
 
-def test_fastareader_keeplinebreaks():
-	with seqio.FastaReader("tests/data/simple.fasta", keep_linebreaks=True) as f:
-		reads = list(f)
-	assert reads[0] == simple_fasta[0]
-	assert reads[1].sequence == 'SEQUEN\nCE2'
+class TestSequence:
+	@raises(seqio.FormatError)
+	def test_too_many_qualities(self):
+		seqio.Sequence(name="name", sequence="ACGT", qualities="#####")
 
 
-@raises(seqio.FormatError)
-def test_fastq_wrongformat():
-	with seqio.FastqReader("tests/data/withplus.fastq") as f:
-		reads = list(f)
+	@raises(seqio.FormatError)
+	def test_too_many_qualities_colorspace(self):
+		seqio.ColorspaceSequence(name="name", sequence="T0123", qualities="#####")
 
 
-@raises(seqio.FormatError)
-def test_fastq_incomplete():
-	fastq = StringIO("@name\nACGT+\n")
-	with seqio.FastqReader(fastq) as fq:
-		list(fq)
+	@raises(seqio.FormatError)
+	def test_invalid_primer(self):
+		seqio.ColorspaceSequence(name="name", sequence="K0123", qualities="####")
 
 
-@raises(seqio.FormatError)
-def test_too_many_qualities():
-	seqio.Sequence(name="name", sequence="ACGT", qualities="#####")
+class TestFastaQualReader:
+	@raises(seqio.FormatError)
+	def test_mismatching_read_names(self):
+		fasta = StringIO(">name\nACG")
+		qual = StringIO(">nome\n3 5 7")
+		list(seqio.FastaQualReader(fasta, qual))
+
+	@raises(seqio.FormatError)
+	def test_invalid_quality_value(self):
+		fasta = StringIO(">name\nACG")
+		qual = StringIO(">name\n3 xx 7")
+		list(seqio.FastaQualReader(fasta, qual))
 
 
-@raises(seqio.FormatError)
-def test_too_many_qualities_colorspace():
-	seqio.ColorspaceSequence(name="name", sequence="T0123", qualities="#####")
+class TestSeqioOpen:
+	def test_sequence_reader(self):
+		# test the autodetection
+		with seqio.open("tests/data/simple.fastq") as f:
+			reads = list(f)
+		assert reads == simple_fastq
 
+		with seqio.open("tests/data/simple.fasta") as f:
+			reads = list(f)
+		assert reads == simple_fasta
 
-@raises(seqio.FormatError)
-def test_invalid_primer():
-	seqio.ColorspaceSequence(name="name", sequence="K0123", qualities="####")
+		with open("tests/data/simple.fastq") as f:
+			reads = list(seqio.open(f))
+		assert reads == simple_fastq
 
-
-@raises(seqio.FormatError)
-def test_mismatching_read_names():
-	fasta = StringIO(">name\nACG")
-	qual = StringIO(">nome\n3 5 7")
-	list(seqio.FastaQualReader(fasta, qual))
-
-
-@raises(seqio.FormatError)
-def test_invalid_quality_value():
-	fasta = StringIO(">name\nACG")
-	qual = StringIO(">name\n3 xx 7")
-	list(seqio.FastaQualReader(fasta, qual))
-
-
-def test_sequence_reader():
-	# test the autodetection
-	with seqio.open("tests/data/simple.fastq") as f:
-		reads = list(f)
-	assert reads == simple_fastq
-
-	with seqio.open("tests/data/simple.fasta") as f:
-		reads = list(f)
-	assert reads == simple_fasta
-
-	with open("tests/data/simple.fastq") as f:
+		# make the name attribute unavailable
+		f = StringIO(open("tests/data/simple.fastq").read())
 		reads = list(seqio.open(f))
-	assert reads == simple_fastq
+		assert reads == simple_fastq
 
-	# make the name attribute unavailable
-	f = StringIO(open("tests/data/simple.fastq").read())
-	reads = list(seqio.open(f))
-	assert reads == simple_fastq
-
-	f = StringIO(open("tests/data/simple.fasta").read())
-	reads = list(seqio.open(f))
-	assert reads == simple_fasta
-
-
-def test_fasta_context_manager():
-	filename = "tests/data/simple.fasta"
-	with open(filename) as f:
-		assert not f.closed
+		f = StringIO(open("tests/data/simple.fasta").read())
 		reads = list(seqio.open(f))
-		assert not f.closed
-	assert f.closed
-
-	with seqio.FastaReader(filename) as sr:
-		tmp_sr = sr
-		assert not sr.fp.closed
-		reads = list(sr)
-		assert not sr.fp.closed
-	assert tmp_sr.fp is None
+		assert reads == simple_fasta
 
 
-def test_fastq_context_manager():
-	filename = "tests/data/simple.fastq"
-	with open(filename) as f:
-		assert not f.closed
-		reads = list(seqio.open(f))
-		assert not f.closed
-	assert f.closed
+class TestInterleavedReader:
 
-	with seqio.FastqReader(filename) as sr:
-		tmp_sr = sr
-		assert not sr.fp.closed
-		reads = list(sr)
-		assert not sr.fp.closed
-	assert tmp_sr.fp is None
+	def test(self):
+		expected = [
+			(seqio.Sequence('read1/1 some text', 'TTATTTGTCTCCAGC', '##HHHHHHHHHHHHH'),
+			seqio.Sequence('read1/2 other text', 'GCTGGAGACAAATAA', 'HHHHHHHHHHHHHHH')),
+			(seqio.Sequence('read3/1', 'CCAACTTGATATTAATAACA', 'HHHHHHHHHHHHHHHHHHHH'),
+			seqio.Sequence('read3/2', 'TGTTATTAATATCAAGTTGG', '#HHHHHHHHHHHHHHHHHHH'))
+		]
+		reads = list(seqio.InterleavedSequenceReader("tests/cut/interleaved.fastq"))
+		for (r1, r2), (e1, e2) in zip(reads, expected):
+			print(r1, r2, e1, e2)
 
-
-def test_interleaved_reader():
-	expected = [
-		(seqio.Sequence('read1/1 some text', 'TTATTTGTCTCCAGC', '##HHHHHHHHHHHHH'),
-		seqio.Sequence('read1/2 other text', 'GCTGGAGACAAATAA', 'HHHHHHHHHHHHHHH')),
-		(seqio.Sequence('read3/1', 'CCAACTTGATATTAATAACA', 'HHHHHHHHHHHHHHHHHHHH'),
-		seqio.Sequence('read3/2', 'TGTTATTAATATCAAGTTGG', '#HHHHHHHHHHHHHHHHHHH'))
-	]
-	reads = list(seqio.InterleavedSequenceReader("tests/cut/interleaved.fastq"))
-	for (r1, r2), (e1, e2) in zip(reads, expected):
-		print(r1, r2, e1, e2)
-
-	assert reads == expected
-	with seqio.open("tests/cut/interleaved.fastq", interleaved=True) as f:
-		reads = list(f)
-	assert reads == expected
+		assert reads == expected
+		with seqio.open("tests/cut/interleaved.fastq", interleaved=True) as f:
+			reads = list(f)
+		assert reads == expected
