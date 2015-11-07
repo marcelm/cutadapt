3701ef066847734400e8436ac484347971379eaf
Marcel Martin <marcel.martin@scilifelab.se>
2015-11-07 22:52:42 +0100

    Move AdapterCutter to modifiers module

--- cutadapt/modifiers.py
+++ cutadapt/modifiers.py
@@ -11,6 +11,150 @@ from cutadapt.qualtrim import quality_trim_index
 from cutadapt.compat import maketrans
 
 
+class AdapterCutter(object):
+	"""
+	Repeatedly find one of multiple adapters in reads.
+	The number of times the search is repeated is specified by the
+	times parameter.
+	"""
+
+	def __init__(self, adapters, times=1, wildcard_file=None, info_file=None,
+			rest_writer=None, action='trim'):
+		"""
+		adapters -- list of Adapter objects
+
+		action -- What to do with a found adapter: None, 'trim', or 'mask'
+		"""
+		self.adapters = adapters
+		self.times = times
+		self.wildcard_file = wildcard_file
+		self.info_file = info_file
+		self.rest_writer = rest_writer
+		self.action = action
+		self.reads_matched = 0
+
+	def _best_match(self, read):
+		"""
+		Find the best matching adapter in the given read.
+
+		Return either an AdapterMatch instance or None if there are no matches.
+		"""
+		best = None
+		for adapter in self.adapters:
+			match = adapter.match_to(read)
+			if match is None:
+				continue
+
+			# the no. of matches determines which adapter fits best
+			if best is None or match.matches > best.matches:
+				best = match
+		return best
+
+	def _write_info(self, read, matches):
+		"""
+		Write to the info, wildcard and rest files.
+		# TODO
+		# This design with a read having a .match attribute and
+		# a match having a .read attribute is really confusing.
+		"""
+		match = read.match
+		if self.rest_writer and match:
+			self.rest_writer.write(match)
+
+		if self.wildcard_file and match:
+			print(match.wildcards(), read.name, file=self.wildcard_file)
+
+		if self.info_file:
+			if match:
+				for m in matches:
+					seq = m.read.sequence
+					qualities = m.read.qualities
+					if qualities is None:
+						qualities = ''
+					print(
+						m.read.name,
+						m.errors,
+						m.rstart,
+						m.rstop,
+						seq[0:m.rstart],
+						seq[m.rstart:m.rstop],
+						seq[m.rstop:],
+						m.adapter.name,
+						qualities[0:m.rstart],
+						qualities[m.rstart:m.rstop],
+						qualities[m.rstop:],
+						sep='\t', file=self.info_file
+					)
+			else:
+				seq = read.sequence
+				qualities = read.qualities if read.qualities is not None else ''
+				print(read.name, -1, seq, qualities, sep='\t', file=self.info_file)
+
+	def __call__(self, read):
+		"""
+		Determine the adapter that best matches the given read.
+		Since the best adapter is searched repeatedly, a list
+		of AdapterMatch instances is returned, which
+		need to be applied consecutively to the read.
+		The list is empty if there are no adapter matches.
+
+		The read is converted to uppercase before it is compared to the adapter
+		sequences.
+
+		Cut found adapters from a single read. Return modified read.
+		"""
+		matches = []
+
+		# try at most self.times times to remove an adapter
+		trimmed_read = read
+		for t in range(self.times):
+			match = self._best_match(trimmed_read)
+			if match is None:
+				# nothing found
+				break
+			assert match.length > 0
+			assert match.errors / match.length <= match.adapter.max_error_rate
+			assert match.length - match.errors > 0
+			matches.append(match)
+			trimmed_read = match.adapter.trimmed(match)
+
+		trimmed_read.match = matches[-1] if matches else None
+		self._write_info(trimmed_read, matches)
+
+		if not matches:
+			return trimmed_read
+
+		if __debug__:
+			assert len(trimmed_read) < len(read), "Trimmed read isn't shorter than original"
+
+		if self.action == 'trim':
+			# read is already trimmed, nothing to do
+			pass
+		elif self.action == 'mask':
+			# add N from last modification
+			masked_sequence = trimmed_read.sequence
+			for match in sorted(matches, reverse=True, key=lambda m: m.astart):
+				ns = 'N' * (len(match.read.sequence) -
+							len(match.adapter.trimmed(match).sequence))
+				# add N depending on match position
+				if match.front:
+					masked_sequence = ns + masked_sequence
+				else:
+					masked_sequence += ns
+			# set masked sequence as sequence with original quality
+			trimmed_read.sequence = masked_sequence
+			trimmed_read.qualities = matches[0].read.qualities
+
+			assert len(trimmed_read.sequence) == len(read)
+		elif self.action is None:
+			trimmed_read = read
+			trimmed_read.match = matches[-1]
+
+		self.reads_matched += 1  # TODO move to filter class
+
+		return trimmed_read
+
+
 class UnconditionalCutter(object):
 	"""
 	A modifier that unconditionally removes the first n or the last n bases from a read.
