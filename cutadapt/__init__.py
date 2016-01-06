e70818ae8ba3a8ca8955e79c60216bd6241a9f79
Marcel Martin <marcel.martin@scilifelab.se>
2016-01-06 15:49:17 +0100

    dev version

--- cutadapt/__init__.py
+++ cutadapt/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import print_function, division, absolute_import
 import sys
 
-__version__ = '1.9.1'
+__version__ = '1.9.2.dev0'
 
 def check_importability():  # pragma: no cover
 	try:
