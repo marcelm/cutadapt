eda18979abb4610ccc33e38520f5e994d8742517
John Didion <github@didion.net>
2016-03-16 12:18:13 -0400

    add Modifiers tests

--- tests/testmodifiers.py
+++ tests/testmodifiers.py
@@ -2,7 +2,7 @@
 from __future__ import print_function, division, absolute_import
 
 from cutadapt.seqio import Sequence
-from cutadapt.modifiers import ModType, UnconditionalCutter, NEndTrimmer, QualityTrimmer
+from cutadapt.modifiers import ModType, Modifiers, UnconditionalCutter, NEndTrimmer, QualityTrimmer
 
 def test_unconditional_cutter():
 	uc = UnconditionalCutter(length=5)
@@ -41,3 +41,37 @@ def test_mod_type():
 	assert ModType[ModType.CUT] == UnconditionalCutter
 	uc = ModType.create_modifier(ModType.CUT, length=2)
 	assert uc.__class__ == UnconditionalCutter
+
+
+def test_Modifiers_single():
+	m = Modifiers()
+	m.add_modifier(ModType.CUT, length=5)
+	assert len(m.mod1) == 1
+	assert list(m.mod1.keys())[0] == ModType.CUT
+	assert list(m.mod1.values())[0].__class__ == UnconditionalCutter
+	assert len(m.mod2) == 0
+	
+	# test single-end
+	read = Sequence('read1', 'ACGTTTACGTA', '##456789###')
+	mod_read = m.modify(read)
+	assert mod_read.sequence == 'TACGTA'
+	
+	# test legacy paired-end
+	read2 = Sequence('read1', 'ACGTTTACGTA', '##456789###')
+	mod_read, mod_read2 = m.modify(read, read2)
+	assert mod_read.sequence == 'TACGTA'
+	assert mod_read2.sequence == 'ACGTTTACGTA'
+
+
+def test_Modifiers_paired_both():
+	m = Modifiers()
+	m.add_modifier(ModType.CUT, read=1|2, length=5)
+	assert len(m.mod1) == 1
+	assert len(m.mod2) == 1
+	assert list(m.mod1.keys())[0] == list(m.mod2.keys())[0] == ModType.CUT
+	assert list(m.mod1.values())[0].__class__ == list(m.mod2.values())[0].__class__  == UnconditionalCutter
+	read = Sequence('read1', 'ACGTTTACGTA', '##456789###')
+	read2 = Sequence('read1', 'ACGTTTACGTA', '##456789###')
+	mod_read, mod_read2 = m.modify(read, read2)
+	assert mod_read.sequence == 'TACGTA'
+	assert mod_read2.sequence == 'TACGTA'
