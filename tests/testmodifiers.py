3ba9f5ec20d6b1ac8be06397e2a34cfd634491d9
John Didion <github@didion.net>
2016-03-16 12:26:44 -0400

    Change UnconditionalCutter and SuffixRemover to operate on lists of lengths/suffixes, so that only one Modifier needs to be created for each type

--- tests/testmodifiers.py
+++ tests/testmodifiers.py
@@ -5,12 +5,12 @@ from cutadapt.seqio import Sequence
 from cutadapt.modifiers import ModType, Modifiers, UnconditionalCutter, NEndTrimmer, QualityTrimmer
 
 def test_unconditional_cutter():
-	uc = UnconditionalCutter(length=5)
+	uc = UnconditionalCutter(lengths=[5])
 	s = 'abcdefg'
-	assert UnconditionalCutter(length=2)(s) == 'cdefg'
-	assert UnconditionalCutter(length=-2)(s) == 'abcde'
-	assert UnconditionalCutter(length=100)(s) == ''
-	assert UnconditionalCutter(length=-100)(s) == ''
+	assert UnconditionalCutter(lengths=[2])(s) == 'cdefg'
+	assert UnconditionalCutter(lengths=[-2])(s) == 'abcde'
+	assert UnconditionalCutter(lengths=[100])(s) == ''
+	assert UnconditionalCutter(lengths=[-100])(s) == ''
 
 
 def test_nend_trimmer():
@@ -39,13 +39,13 @@ def test_quality_trimmer():
 def test_mod_type():
 	assert ModType.ADAPTER == "ADAPTER"
 	assert ModType[ModType.CUT] == UnconditionalCutter
-	uc = ModType.create_modifier(ModType.CUT, length=2)
+	uc = ModType.create_modifier(ModType.CUT, lengths=[2])
 	assert uc.__class__ == UnconditionalCutter
 
 
 def test_Modifiers_single():
 	m = Modifiers()
-	m.add_modifier(ModType.CUT, length=5)
+	m.add_modifier(ModType.CUT, lengths=[5])
 	assert len(m.mod1) == 1
 	assert list(m.mod1.keys())[0] == ModType.CUT
 	assert list(m.mod1.values())[0].__class__ == UnconditionalCutter
@@ -65,7 +65,7 @@ def test_Modifiers_single():
 
 def test_Modifiers_paired_both():
 	m = Modifiers()
-	m.add_modifier(ModType.CUT, read=1|2, length=5)
+	m.add_modifier(ModType.CUT, read=1|2, lengths=[5])
 	assert len(m.mod1) == 1
 	assert len(m.mod2) == 1
 	assert list(m.mod1.keys())[0] == list(m.mod2.keys())[0] == ModType.CUT
