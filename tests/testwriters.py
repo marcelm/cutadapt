a7148d3aef2291817d7755b5904fa478a8107cb4
John Didion <github@didion.net>
2016-04-10 14:21:51 -0400

    make tests work with python 2.6

--- tests/testwriters.py
+++ tests/testwriters.py
@@ -25,61 +25,63 @@ def test_single_end():
 
 def test_paired_end():
 	w = writers.Writers()
-	with temporary_path("result1.fq") as path1, temporary_path("result2.fq") as path2:
-		w.add_writer(None, path1, path2)
-		assert w.has_writer(None)
-		assert isinstance(w.get_writer(None), writers.PairedEndWriter)
-		read1 = Sequence(
-			name="read1/1 some text", 
-			sequence="TTATTTGTCTCCAGCTTAGACATATCGCCT",
-			qualities="##HHHHHHHHHHHHHHHHHHHHHHHHHHHH"
-		)
-		read2 = Sequence(
-			name="read1/2 other text", 
-			sequence="GCTGGAGACAAATAACAGTGGAGTAGTTTT",
-			qualities="HHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
-		)
-		w.write(None, read1, read2)
-		w.close()
-		assert files_equal(datapath('small.paired.1.fastq'), path1)
-		assert files_equal(datapath('small.paired.2.fastq'), path2)
-		summary = w.summary()
-		assert summary[0] == 1
-		assert summary[1][0] == 30
-		assert summary[1][1] == 30
+	with temporary_path("result1.fq") as path1:
+		with temporary_path("result2.fq") as path2:
+			w.add_writer(None, path1, path2)
+			assert w.has_writer(None)
+			assert isinstance(w.get_writer(None), writers.PairedEndWriter)
+			read1 = Sequence(
+				name="read1/1 some text", 
+				sequence="TTATTTGTCTCCAGCTTAGACATATCGCCT",
+				qualities="##HHHHHHHHHHHHHHHHHHHHHHHHHHHH"
+			)
+			read2 = Sequence(
+				name="read1/2 other text", 
+				sequence="GCTGGAGACAAATAACAGTGGAGTAGTTTT",
+				qualities="HHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
+			)
+			w.write(None, read1, read2)
+			w.close()
+			assert files_equal(datapath('small.paired.1.fastq'), path1)
+			assert files_equal(datapath('small.paired.2.fastq'), path2)
+			summary = w.summary()
+			assert summary[0] == 1
+			assert summary[1][0] == 30
+			assert summary[1][1] == 30
 
 def test_multiplexed():
 	w = writers.Writers(multiplexed=True, name_pattern=os.path.join(temporary_directory(), "foo.{name}.fa"))
-	with temporary_path("foo.alpha.fa") as path1, temporary_path("foo.beta.fa") as path2:
-		assert isinstance(w.get_multiplex_writer("alpha"), writers.SingleEndWriter)
-		assert isinstance(w.get_multiplex_writer("beta"), writers.SingleEndWriter)
+	with temporary_path("foo.alpha.fa") as path1:
+		with temporary_path("foo.beta.fa") as path2:
+			assert isinstance(w.get_multiplex_writer("alpha"), writers.SingleEndWriter)
+			assert isinstance(w.get_multiplex_writer("beta"), writers.SingleEndWriter)
 		
-		class DummyAdapter(object):
-			def __init__(self, name):
-				self.name = name
-		class DummyMatch(object):
-			def __init__(self, name):
-				self.adapter = DummyAdapter(name)
+			class DummyAdapter(object):
+				def __init__(self, name):
+					self.name = name
+			class DummyMatch(object):
+				def __init__(self, name):
+					self.adapter = DummyAdapter(name)
 
-		read1 = Sequence(
-			name="read1",
-			sequence="GATCCTCCTGGAGCTGGCTGATACCAGTATACCAGTGCTGATTGTTG",
-			match = DummyMatch("alpha")
-		)
-		w.write(None, read1)
-		read2 = Sequence(
-			name="read2",
-			sequence="CTCGAGAATTCTGGATCCTCTCTTCTGCTACCTTTGGGATTTGCTTGCTCTTG",
-			match=DummyMatch("beta")
-		)
-		w.write(None, read2)
-		w.close()
-		assert files_equal(cutpath('twoadapters.first.fasta'), path1)
-		assert files_equal(cutpath('twoadapters.second.fasta'), path2)
-		summary = w.summary()
-		assert summary[0] == 2
-		assert summary[1][0] == 100
-		assert summary[1][1] == 0
+			read1 = Sequence(
+				name="read1",
+				sequence="GATCCTCCTGGAGCTGGCTGATACCAGTATACCAGTGCTGATTGTTG",
+				match = DummyMatch("alpha")
+			)
+			w.write(None, read1)
+			read2 = Sequence(
+				name="read2",
+				sequence="CTCGAGAATTCTGGATCCTCTCTTCTGCTACCTTTGGGATTTGCTTGCTCTTG",
+				match=DummyMatch("beta")
+			)
+			w.write(None, read2)
+			w.close()
+			assert files_equal(cutpath('twoadapters.first.fasta'), path1)
+			assert files_equal(cutpath('twoadapters.second.fasta'), path2)
+			summary = w.summary()
+			assert summary[0] == 2
+			assert summary[1][0] == 100
+			assert summary[1][1] == 0
 
 def test_force_create():
 	w = writers.Writers()
