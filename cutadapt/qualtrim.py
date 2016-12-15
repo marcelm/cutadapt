848a0b207eeea31b59b891017aa479606bd63843
Marcel Martin <marcel.martin@scilifelab.se>
2016-12-15 08:53:21 +0100

    Remove outdated Python version of quality_trim_index()

--- cutadapt/qualtrim.py
+++ cutadapt/qualtrim.py
@@ -10,32 +10,6 @@ if sys.version > '3':
 	xrange = range
 
 
-def quality_trim_index(qualities, cutoff, base=33):
-	"""
-	Find the position at which to trim a low-quality end from a nucleotide sequence.
-
-	Qualities are assumed to be ASCII-encoded as chr(qual + base).
-
-	The algorithm is the same as the one used by BWA within the function
-	'bwa_trim_read':
-	- Subtract the cutoff value from all qualities.
-	- Compute partial sums from all indices to the end of the sequence.
-	- Trim sequence at the index at which the sum is minimal.
-	"""
-	s = 0
-	max_qual = 0
-	max_i = len(qualities)
-	for i in reversed(xrange(max_i)):
-		q = ord(qualities[i]) - base
-		s += cutoff - q
-		if s < 0:
-			break
-		if s > max_qual:
-			max_qual = s
-			max_i = i
-	return max_i
-
-
 def nextseq_trim_index(sequence, cutoff, base=33):
 	"""
 	Variant of the above quality trimming routine that works on NextSeq data.
@@ -64,7 +38,5 @@ def nextseq_trim_index(sequence, cutoff, base=33):
 			max_i = i
 	return max_i
 
-try:
-	from cutadapt._qualtrim import quality_trim_index, nextseq_trim_index
-except:
-	pass
+
+from cutadapt._qualtrim import quality_trim_index, nextseq_trim_index
