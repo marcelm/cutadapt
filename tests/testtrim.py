67439460741966ec779c62407b9077ff75862f80
Tim Booth <tim.booth@ed.ac.uk>
2017-04-26 17:42:17 +0100

    Stylistic fixes as requested by @marcelm

--- tests/testtrim.py
+++ tests/testtrim.py
@@ -27,12 +27,14 @@ def test_statistics():
 			trimmed_bp += sum(seqlen * count for (seqlen, count) in d.items())
 	assert trimmed_bp <= len(read), trimmed_bp
 
+
 def test_end_trim_with_mismatch():
-    """ Test the not-so-obvious case where an adapter of length 13 is trimmed from
-        the end of a sequence with overlap 9 and there is one deletion.
-        In this case the algorithm starts with 10 bases of the adapter to get
-        the hit and so the match is considered good. An insertion or substitution
-        at the same spot is not a match.
+    """
+    Test the not-so-obvious case where an adapter of length 13 is trimmed from
+    the end of a sequence with overlap 9 and there is one deletion.
+    In this case the algorithm starts with 10 bases of the adapter to get
+    the hit and so the match is considered good. An insertion or substitution
+    at the same spot is not a match.
     """
     adapter = Adapter('TCGATCGATCGAT', BACK, 0.1)
 
@@ -42,8 +44,8 @@ def test_end_trim_with_mismatch():
 
     assert trimmed_read.sequence == 'AAAAAAAAAAA'
     assert cutter.adapter_statistics[adapter].lengths_back == {9: 1}
-    #We see 1 error at length 9 even though the number of allowed mismatches at
-    #length 9 is 0.
+    # We see 1 error at length 9 even though the number of allowed mismatches at
+    # length 9 is 0.
     assert cutter.adapter_statistics[adapter].errors_back[9][1] == 1
 
     read = Sequence('foo2', 'AAAAAAAAAAATCGAACGA')
