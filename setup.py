a801765a1eb18ee01b9f7e4f1d303bba34702f43
Marcel Martin <marcel.martin@scilifelab.se>
2019-08-21 20:28:04 +0200

    Pin dependencies
    Use "compatible release" version specifiers for the dnaio and xopen
    dependencies. Not using the ">=" operator should avoid suprises
    when dependencies are updated.
    
    See https://www.python.org/dev/peps/pep-0440/#compatible-release
    
    Close #384

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ setup(
     package_dir={'': 'src'},
     packages=find_packages('src'),
     entry_points={'console_scripts': ['cutadapt = cutadapt.__main__:main']},
-    install_requires=['dnaio>=0.3', 'xopen>=0.7.3'],
+    install_requires=['dnaio~=0.3.0', 'xopen~=0.8.1'],
     extras_require={
         'dev': ['Cython', 'pytest', 'pytest-timeout', 'sphinx', 'sphinx_issues'],
     },
