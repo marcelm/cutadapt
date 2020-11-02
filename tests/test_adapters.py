c830e24904a1fe7d0e7118dc8d4c00424b6a3d9f
Marcel Martin <marcel.martin@scilifelab.se>
2020-11-02 22:28:44 +0100

    Rename MultiAdapter -> IndexedAdapters

--- tests/test_adapters.py
+++ tests/test_adapters.py
@@ -8,8 +8,8 @@ from cutadapt.adapters import (
     PrefixAdapter,
     SuffixAdapter,
     LinkedAdapter,
-    MultiPrefixAdapter,
-    MultiSuffixAdapter,
+    IndexedPrefixAdapters,
+    IndexedSuffixAdapters,
 )
 
 
@@ -289,41 +289,41 @@ def test_suffix_match_with_n_wildcard_in_read():
     assert match is not None and (4, 11) == (match.rstart, match.rstop)
 
 
-def test_multi_prefix_adapter():
+def test_indexed_prefix_adapters():
     adapters = [
         PrefixAdapter("GAAC", indels=False),
         PrefixAdapter("TGCT", indels=False),
     ]
-    ma = MultiPrefixAdapter(adapters)
+    ma = IndexedPrefixAdapters(adapters)
     match = ma.match_to("GAACTT")
     assert match.adapter is adapters[0]
     match = ma.match_to("TGCTAA")
     assert match.adapter is adapters[1]
 
 
-def test_multi_prefix_adapter_incorrect_type():
+def test_indexed_prefix_adapters_incorrect_type():
     with pytest.raises(ValueError):
-        MultiPrefixAdapter([
+        IndexedPrefixAdapters([
             PrefixAdapter("GAAC", indels=False),
             SuffixAdapter("TGCT", indels=False),
         ])
 
 
-def test_multi_suffix_adapter():
+def test_indexed_suffix_adapters():
     adapters = [
         SuffixAdapter("GAAC", indels=False),
         SuffixAdapter("TGCT", indels=False),
     ]
-    ma = MultiSuffixAdapter(adapters)
+    ma = IndexedSuffixAdapters(adapters)
     match = ma.match_to("TTGAAC")
     assert match.adapter is adapters[0]
     match = ma.match_to("AATGCT")
     assert match.adapter is adapters[1]
 
 
-def test_multi_suffix_adapter_incorrect_type():
+def test_indexed_suffix_adapters_incorrect_type():
     with pytest.raises(ValueError):
-        MultiSuffixAdapter([
+        IndexedSuffixAdapters([
             SuffixAdapter("GAAC", indels=False),
             PrefixAdapter("TGCT", indels=False),
         ])
