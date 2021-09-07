58785fa4cb39c676084ef3898b4b82a7fbe8bdb9
Marcel Martin <marcel.martin@scilifelab.se>
2021-09-07 17:13:06 +0200

    Better error message for "I" (inosine) in adapter sequence
    See #546

--- src/cutadapt/adapters.py
+++ src/cutadapt/adapters.py
@@ -540,9 +540,15 @@ class SingleAdapter(Adapter, ABC):
         if adapter_wildcards and not set(self.sequence) <= iupac:
             for c in self.sequence:
                 if c not in iupac:
-                    raise InvalidCharacter('Character {!r} in adapter sequence {!r} is '
-                        'not a valid IUPAC code. Use only characters '
-                        'ABCDGHKMNRSTUVWXY.'.format(c, self.sequence))
+                    if c == "I":
+                        extra = "For inosine, consider using N instead and please comment " \
+                                "on <https://github.com/marcelm/cutadapt/issues/546>."
+                    else:
+                        extra = "Use only characters 'ABCDGHKMNRSTUVWXY'."
+                    raise InvalidCharacter(
+                        f"Character '{c}' in adapter sequence '{self.sequence}' is "
+                        f"not a valid IUPAC code. {extra}"
+                    )
         # Optimization: Use non-wildcard matching if only ACGT is used
         self.adapter_wildcards: bool = adapter_wildcards and not set(self.sequence) <= set("ACGT")
         self.read_wildcards: bool = read_wildcards
