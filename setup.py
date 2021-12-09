a650fb75cecdda3ae3332c46dca34198fe206861
Marcel Martin <marcel.martin@scilifelab.se>
2021-12-09 10:52:03 +0100

    Simplify setup.py by always cythonizing
    We already have Cython as a build dependency in pyproject.toml, so we can
    rely on it being available.

--- setup.py
+++ setup.py
@@ -2,34 +2,12 @@
 Build Cutadapt.
 """
 import sys
-import os.path
 
 from setuptools import setup, Extension, find_packages
 from distutils.version import LooseVersion
-from setuptools.command.sdist import sdist
-from setuptools.command.build_ext import build_ext
+from Cython.Build import cythonize
 
 MIN_CYTHON_VERSION = '0.28'
-
-
-def no_cythonize(extensions, **_ignore):
-    """
-    Change file extensions from .pyx to .c or .cpp.
-
-    Copied from Cython documentation
-    """
-    for extension in extensions:
-        sources = []
-        for sfile in extension.sources:
-            path, ext = os.path.splitext(sfile)
-            if ext in ('.pyx', '.py'):
-                if extension.language == 'c++':
-                    ext = '.cpp'
-                else:
-                    ext = '.c'
-                sfile = path + ext
-            sources.append(sfile)
-        extension.sources[:] = sources
 
 
 def check_cython_version():
@@ -48,34 +26,14 @@ def check_cython_version():
         sys.exit(1)
 
 
-class BuildExt(build_ext):
-    def run(self):
-        # If we encounter a PKG-INFO file, then this is likely a .tar.gz/.zip
-        # file retrieved from PyPI that already includes the pre-cythonized
-        # extension modules, and then we do not need to run cythonize().
-        if os.path.exists('PKG-INFO'):
-            no_cythonize(self.extensions)
-        else:
-            # Otherwise, this is a 'developer copy' of the code, and then the
-            # only sensible thing is to require Cython to be installed.
-            check_cython_version()
-            from Cython.Build import cythonize
-            self.extensions = cythonize(self.extensions)
-        super().run()
-
-
-class SDist(sdist):
-    def run(self):
-        # Make sure the compiled Cython files in the distribution are up-to-date
-        from Cython.Build import cythonize
-        check_cython_version()
-        cythonize(self.distribution.ext_modules)
-        super().run()
-
-
 encoding_arg = {'encoding': 'utf-8'} if sys.version_info[0] >= 3 else dict()
 with open('README.rst', **encoding_arg) as f:
     long_description = f.read()
+
+extensions = [
+    Extension('cutadapt._align', sources=['src/cutadapt/_align.pyx']),
+    Extension('cutadapt.qualtrim', sources=['src/cutadapt/qualtrim.pyx']),
+]
 
 setup(
     name='cutadapt',
@@ -88,11 +46,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/x-rst',
     license='MIT',
-    cmdclass={'build_ext': BuildExt, 'sdist': SDist},
-    ext_modules=[
-        Extension('cutadapt._align', sources=['src/cutadapt/_align.pyx']),
-        Extension('cutadapt.qualtrim', sources=['src/cutadapt/qualtrim.pyx']),
-    ],
+    ext_modules=cythonize(extensions),
     package_dir={'': 'src'},
     packages=find_packages('src'),
     entry_points={'console_scripts': ['cutadapt = cutadapt.__main__:main_cli']},
