df45ea4e89a96fcfff2203b4b77fe8b75ba696dd
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-12-23 13:03:54 +0100

    Implement 'shift AND' search algorithm

--- src/cutadapt/_kmer_finder.pyx
+++ src/cutadapt/_kmer_finder.pyx
@@ -1,15 +1,20 @@
 # cython: language_level=3
 
 from cpython.mem cimport PyMem_Malloc, PyMem_Free
-from libc.string cimport memcpy, strstr
+from libc.string cimport memset
 from cpython.unicode cimport PyUnicode_CheckExact, PyUnicode_GET_LENGTH
+from libc.stdint cimport uint8_t
+
+# Dnaio conveniently ensures that all sequences are ASCII only.
+DEF ASCII_CHAR_COUNT = 128
 
 cdef extern from "Python.h":
     void *PyUnicode_DATA(object o)
     bint PyUnicode_IS_COMPACT_ASCII(object o)
 
 ctypedef struct KmerEntry:
-    size_t kmer_offset
+    size_t kmer_length
+    size_t mask_offset
     ssize_t search_offset
 
 
@@ -32,13 +37,13 @@ cdef class KmerFinder:
     happens only once.
     """
     cdef:
-        char *kmers
         KmerEntry *kmer_entries
+        size_t *kmer_masks
         size_t number_of_kmers
         object kmers_and_offsets
 
     def __cinit__(self, kmers_and_offsets):
-        self.kmers = NULL
+        self.kmer_masks = NULL
         self.kmer_entries = NULL
         self.number_of_kmers = 0
         kmers = [kmer for kmer, _ in kmers_and_offsets]
@@ -46,9 +51,9 @@ cdef class KmerFinder:
         number_of_entries = len(kmers_and_offsets)
         self.kmer_entries = <KmerEntry *>PyMem_Malloc(number_of_entries * sizeof(KmerEntry))
         # for the kmers the NULL bytes also need space.
-        self.kmers = <char *>PyMem_Malloc(kmer_total_length + number_of_entries)
+        self.kmer_masks = <size_t *>PyMem_Malloc(number_of_entries * sizeof(size_t) * ASCII_CHAR_COUNT)
         self.number_of_kmers = number_of_entries
-        cdef size_t kmer_offset = 0
+        cdef size_t mask_offset = 0
         cdef char *kmer_ptr
         cdef Py_ssize_t kmer_length
         for i, (kmer, offset) in enumerate(kmers_and_offsets):
@@ -56,16 +61,13 @@ cdef class KmerFinder:
                 raise TypeError(f"Kmer should be a string not {type(kmer)}")
             if not PyUnicode_IS_COMPACT_ASCII(kmer):
                 raise ValueError("Only ASCII strings are supported")
-            self.kmer_entries[i].kmer_offset = kmer_offset
             self.kmer_entries[i].search_offset  = offset
+            self.kmer_entries[i].mask_offset = mask_offset
             kmer_length = PyUnicode_GET_LENGTH(kmer)
+            self.kmer_entries[i].kmer_length = kmer_length
             kmer_ptr = <char *>PyUnicode_DATA(kmer)
-            memcpy(self.kmers + kmer_offset, kmer_ptr, kmer_length)
-            kmer_offset += kmer_length
-            # NULL terminate each kmer in the kmer string as strstr only works
-            # on NULL terminated strings.
-            self.kmers[kmer_offset] = 0
-            kmer_offset += 1
+            populate_needle_mask(self.kmer_masks + mask_offset, kmer_ptr, kmer_length)
+            mask_offset += ASCII_CHAR_COUNT
         self.kmers_and_offsets = kmers_and_offsets
 
     def __reduce__(self):
@@ -76,10 +78,12 @@ cdef class KmerFinder:
             KmerEntry entry
             size_t i
             size_t kmer_offset
+            size_t kmer_length
             ssize_t search_offset
-            char *kmer_ptr
+            size_t *mask_ptr
             char *search_ptr
             char *search_result
+            size_t search_length
         if not PyUnicode_IS_COMPACT_ASCII(sequence):
             raise ValueError("Only ASCII strings are supported")
         cdef char *seq = <char *>PyUnicode_DATA(sequence)
@@ -93,15 +97,44 @@ cdef class KmerFinder:
                     search_offset = 0
             if search_offset > seq_length:
                 continue
-            kmer_offset = entry.kmer_offset
-            kmer_ptr = self.kmers + kmer_offset
+            kmer_length = entry.kmer_length
+            mask_ptr = self.kmer_masks + entry.mask_offset
             search_ptr = seq + search_offset
-            # memmem is 10-15% faster than strstr, but is not a C standard function.
-            search_result = strstr(search_ptr, kmer_ptr)
+            search_length = seq_length - (search_ptr - seq)
+            search_result = shift_and_search(search_ptr, search_length,
+                                             mask_ptr, kmer_length)
             if search_result:
                 return True
         return False
 
     def __dealloc__(self):
-        PyMem_Free(self.kmers)
+        PyMem_Free(self.kmer_masks)
         PyMem_Free(self.kmer_entries)
+
+
+cdef populate_needle_mask(size_t *needle_mask, char *needle, size_t needle_length):
+    cdef size_t i
+    if needle_length > (sizeof(size_t) * 8 - 1):
+        raise ValueError("The pattern is too long!")
+    memset(needle_mask, 0xff, sizeof(size_t) * ASCII_CHAR_COUNT)
+    for i in range(needle_length):
+        needle_mask[<uint8_t>needle[i]] &= ~(1UL << i)
+
+
+cdef char *shift_and_search(char *haystack, size_t haystack_length,
+                            size_t *needle_mask, size_t needle_length):
+    cdef:
+        size_t R = ~1
+        size_t i
+
+    if needle_length == 0:
+        return haystack
+
+    for i in range(haystack_length):
+        # Update the bit array
+        R |= needle_mask[<uint8_t>haystack[i]]
+        R <<= 1
+        if (0 == (R & (1UL << needle_length))):
+            return (haystack + i - needle_length) + 1
+
+    return NULL
