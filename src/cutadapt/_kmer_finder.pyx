03754a3d28ab0ad24e89ceaf05ab3a7ce6633efb
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-12-28 16:49:53 +0100

    Mark mutually exclusive conditions with elif

--- src/cutadapt/_kmer_finder.pyx
+++ src/cutadapt/_kmer_finder.pyx
@@ -103,13 +103,13 @@ cdef class KmerFinder:
                 start = seq_length + start
                 if start < 0:
                     start = 0
-            if start > seq_length:
+            elif start > seq_length:
                 continue
             if stop < 0:
                 step = seq_length + stop
                 if stop <= 0:  # No need to search
                     continue
-            if stop == 0:
+            elif stop == 0:
                 stop = seq_length
             search_length = stop - start
             if search_length <= 0:
