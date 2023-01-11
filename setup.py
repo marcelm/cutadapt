746892765024918e2dc93c40832aee9f955fcd0e
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-01-11 13:33:31 +0100

    Add metrics file to modules

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ extensions = [
     Extension("cutadapt._align", sources=["src/cutadapt/_align.pyx"]),
     Extension("cutadapt.qualtrim", sources=["src/cutadapt/qualtrim.pyx"]),
     Extension("cutadapt.info", sources=["src/cutadapt/info.pyx"]),
+    Extension("cutadapt._qc", sources=["src/cutadapt/_qc.pyx"]),
 ]
 
 setup(ext_modules=extensions)
