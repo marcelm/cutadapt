e7eab780136483f396f811755a760d4afbbabe5c
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-01-13 09:35:09 +0100

    Apply black formatting

--- src/cutadapt/qc.py
+++ src/cutadapt/qc.py
@@ -9,7 +9,7 @@ import pygal
 from ._qc import NUMBER_OF_NUCS, NUMBER_OF_PHREDS, QCMetrics
 
 N, A, C, G, T = 0, 1, 2, 3, 4
-PHRED_TO_ERROR_RATE = [10 ** (-p /10) for p in range(NUMBER_OF_PHREDS)]
+PHRED_TO_ERROR_RATE = [10 ** (-p / 10) for p in range(NUMBER_OF_PHREDS)]
 
 
 class QCMetricsReport:
@@ -40,7 +40,7 @@ class QCMetricsReport:
         plot = pygal.Line(
             title="Per base sequence quality",
             dots_size=1,
-            x_labels = list(range(1, self.max_length + 1)),
+            x_labels=list(range(1, self.max_length + 1)),
             x_labels_major=list(range(0, self.max_length, 10)),
             show_minor_x_labels=False,
             truncate_label=-1,
@@ -73,7 +73,7 @@ class QCMetricsReport:
         plot = pygal.Line(
             title="Base content",
             dots_size=1,
-            x_labels = list(range(1, self.max_length + 1)),
+            x_labels=list(range(1, self.max_length + 1)),
             x_labels_major=list(range(0, self.max_length, 10)),
             show_minor_x_labels=False,
             truncate_label=-1,
@@ -130,8 +130,7 @@ class QCMetricsReport:
         length_counts[0] = metrics.number_of_reads
         qualities = [[0.0 for _ in range(sequence_length)] for _ in range(5)]
         mean_qualities = [0.0 for _ in range(sequence_length)]
-        base_content = [[0.0 for _ in range(sequence_length)] for _ in
-                        range(5)]
+        base_content = [[0.0 for _ in range(sequence_length)] for _ in range(5)]
         gc_content = [0.0 for _ in range(sequence_length)]
         grand_total_bases = 0
         grand_total_gc = 0
@@ -139,21 +138,18 @@ class QCMetricsReport:
         grand_total_q20 = 0
         grand_total_q30 = 0
 
-        for sequence_pos, table_offset in enumerate(
-            range(0, len(matrix), table_size)):
+        for sequence_pos, table_offset in enumerate(range(0, len(matrix), table_size)):
             error_rates = [0.0 for _ in range(5)]
             base_counts = [0 for _ in range(5)]
-            table = matrix[table_offset:table_offset + table_size]
-            for phred, row_offset in enumerate(
-                range(0, table_size, NUMBER_OF_NUCS)):
-                nucs = table[row_offset:row_offset + NUMBER_OF_NUCS]
+            table = matrix[table_offset : table_offset + table_size]
+            for phred, row_offset in enumerate(range(0, table_size, NUMBER_OF_NUCS)):
+                nucs = table[row_offset : row_offset + NUMBER_OF_NUCS]
                 for n_index, nuc_count in enumerate(nucs):
                     if phred >= 20:
                         grand_total_q20 += nuc_count
                     if phred >= 30:
                         grand_total_q30 += nuc_count
-                    error_rates[n_index] += PHRED_TO_ERROR_RATE[
-                                                phred] * nuc_count
+                    error_rates[n_index] += PHRED_TO_ERROR_RATE[phred] * nuc_count
                     base_counts[n_index] += nuc_count
             total_bases = sum(base_counts)
             for i in range(NUMBER_OF_NUCS):
@@ -161,10 +157,10 @@ class QCMetricsReport:
                 base_content[i][sequence_pos] = base_count / total_bases
                 if base_count:
                     average_error = error_rates[i] / base_count
-                    qualities[i][sequence_pos] = -10 * math.log10(
-                        average_error)
+                    qualities[i][sequence_pos] = -10 * math.log10(average_error)
             mean_qualities[sequence_pos] = -10 * math.log10(
-                sum(error_rates) / total_bases)
+                sum(error_rates) / total_bases
+            )
             at = base_counts[A] + base_counts[T]
             gc = base_counts[G] + base_counts[C]
             # Don't include N in gc content calculation
@@ -178,8 +174,9 @@ class QCMetricsReport:
         previous_count = 0
         for i in range(sequence_length, 0, -1):
             number_of_sequences_at_least_length = length_counts[i]
-            number_of_sequences_exactly_length = \
+            number_of_sequences_exactly_length = (
                 number_of_sequences_at_least_length - previous_count
+            )
             sequence_lengths[i] = number_of_sequences_exactly_length
             previous_count = number_of_sequences_at_least_length
 
@@ -204,4 +201,3 @@ if __name__ == "__main__":  # pragma: no cover
             metrics.add_read(read)
     report = QCMetricsReport(metrics)
     print(report.html_report())
-
